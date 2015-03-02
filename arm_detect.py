0d9abe4ada9535e85645c4da2759376c46502927
byronwasti <byron.wasti@gmail.com>
2015-03-02 15:35:34 -0500

    Still playing around with functionality

--- arm_detect.py
+++ arm_detect.py
@@ -2,12 +2,23 @@
 
 import cv2
 import numpy as np
+import time
 cap = cv2.VideoCapture(0)
 
+print "Step out of frame"
+time.sleep(4)
+print "Taking back"
+for i in xrange(100):
+    ret, BACK = cap.read()
+
+BACK = cv2.cvtColor(BACK,cv2.COLOR_BGR2GRAY)
+print "Step back into frame"
+
+i = 0
 while(True):
     ret, frame = cap.read() 
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    frame = np.float32(frame)
+    #frame = np.float32(frame)
 
     # Display the resulting frame
     '''
@@ -19,14 +30,14 @@ while(True):
     output2 = cv2.flip(frame,1)
     '''
 
-    output3 = frame
-    dst = cv2.cornerHarris(frame,2,3,0.04)
-    dst = cv2.dilate(dst, None)
-    output3[dst>0.01*dst.max()] = [0,0,255]
-
     #cv2.imshow('frame',np.vstack( [np.hstack([frame,output1]), np.hstack([output2,output3])]))
     #cv2.moveWindow('frame',0,0)
-    cv2.imshow('frame',output3)
+    diff = cv2.absdiff(frame,BACK)
+    NPDIFF = frame - BACK
+    cv2.imshow('frame',np.vstack( [np.hstack([BACK,diff]), np.hstack([frame,NPDIFF])] ))
+    if i == 0:
+        i = 1
+        cv2.moveWindow('frame', 0,0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- movement_detect.py
+++ movement_detect.py
@@ -4,16 +4,25 @@ import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
 
+boundaries = [ ([50,30,30], [145,133,128]) ]
+
 while(True):
     ret, frame = cap.read()
 
-    try: diff = cv2.absdiff(frame,frame_old)
+    try: 
+        diff = cv2.absdiff(frame,frame_old)
+        diff_np = frame - frame_old
+        for (lower, upper) in boundaries:
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+            mask = cv2.inRange(diff_np, lower, upper)
+            diff_np = cv2.bitwise_and(frame,frame, mask=mask)
     except: 
         frame_old = frame
         continue
         
     # Display the resulting frame
-    cv2.imshow('frame',diff)
+    cv2.imshow('frame',np.hstack([diff,diff_np]))
 
     frame_old = frame
 
