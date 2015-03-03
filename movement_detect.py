af4e39f814e1b031776bae6189aff6d3618557fb
byronwasti <byron.wasti@gmail.com>
2015-03-03 17:05:53 -0500

    Added a bit of color recognition for the initial frame. Can be used to colorize the bird

--- movement_detect.py
+++ movement_detect.py
@@ -6,10 +6,9 @@ boundaries = [ ([50,30,30], [145,133,128]) ]
 
 starter = 0
 while(True):
-#for x in xrange(3):
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
-    frame = cv2.blur(frame, (5,5) )
+    #frame = cv2.blur(frame, (5,5) )
 
     if starter == 0:
         frame_old = frame
@@ -24,6 +23,7 @@ while(True):
         mask = cv2.inRange(diff_np, lower, upper)
         
     mask = cv2.pyrDown(mask)
+    frame2 = cv2.pyrDown(frame)
     white_x = []
     white_y = []
     for y in xrange( mask.shape[0] ):
@@ -36,10 +36,13 @@ while(True):
     avg_x = sum(white_x) / float(len(white_x)+0.1)
 
     color = cv2.mean(frame)[:3]
+    #color = cv2.max(frame,frame)[:3]
+    #color = cv2.mean(color)[:3]
+    #color = (10,100,10)
     if avg_x != 0 and avg_y != 0:  
-        cv2.circle(mask, (int(avg_x), int(avg_y)), 10, color, -1) 
+        cv2.circle(frame2, (int(avg_x), int(avg_y)), 10, color, -1) 
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', frame2)
     cv2.imshow('mask', mask)
 
     
