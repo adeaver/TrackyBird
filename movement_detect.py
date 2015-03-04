03d7f6e26459a4738ce5eed4080f7691845a6f65
byronwasti <byron.wasti@gmail.com>
2015-03-03 19:38:14 -0500

    Finished and now a module that is super easy to use

--- movement_detect.py
+++ movement_detect.py
@@ -1,56 +1,83 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
 
-boundaries = [ ([50,30,30], [145,133,128]) ]
+class Movement_Track:
+    def __init__(self,color=(0,0,255),video=0):
+        self.cap = cv2.VideoCapture(video)
+        self.boundaries = [ ([50,30,30], [145,133,128]) ]
+        self.starter = 0
+        self.frame_old = []
+        self.color = color
+        self.prev = 0 # previous y position
 
-starter = 0
-while(True):
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    #frame = cv2.blur(frame, (5,5) )
+        ret, frame = self.cap.read()
+        frame = cv2.pyrDown(frame)
+        height = frame.shape[0]
 
-    if starter == 0:
-        frame_old = frame
-        starter = 1
-        continue
+        self.maxy = height/2.0 # maximum y value reached
+        self.miny = height/2.0 # minimum y value reached
+        self.avg = height/2.0 # Initial average value
 
-    diff_np = frame - frame_old
+    def Get_Color(self):
+        ret, frame = self.cap.read()
+        self.color = cv2.mean(frame)[:3]
+        return self.color
 
-    for (lower, upper) in boundaries:
-        lower = np.array(lower, dtype="uint8")
-        upper = np.array(upper, dtype="uint8")
-        mask = cv2.inRange(diff_np, lower, upper)
+    def Movement(self):
+        y = self.Get_Move()
+        if y == 0:
+            return False
+
+        if y > self.maxy:
+            self.maxy = y
+            self.avg = (self.maxy + self.miny)/2.0
+
+        if y < self.miny:
+            self.miny = y
+            self.avg = (self.maxy + self.miny)/2.0
+
+        if y > self.avg and self.prev < self.avg:
+            self.prev = y
+            return True
+        else:
+            self.prev = y
+            return False
+
+    def Get_Move(self):
+        ret, frame = self.cap.read()
+        #frame = cv2.flip(frame, 1)
+
+        if self.starter == 0:
+            self.frame_old = frame
+            self.starter = 1
+            return 0
+
+        diff_np = frame - self.frame_old
+
+        for (lower, upper) in self.boundaries:
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+            mask = cv2.inRange(diff_np, lower, upper)
+            
+        mask = cv2.pyrDown(mask)
+        white_x = []
+        white_y = []
+        for y in xrange( mask.shape[0] ):
+            for x in xrange( mask.shape[1] ):
+                if mask.item(y,x) == 255:
+                    white_x.append(x)
+                    white_y.append(y)
+
+        avg_y = sum(white_y) / float(len(white_y)+0.1)
+        #avg_x = sum(white_x) / float(len(white_x)+0.1)
         
-    mask = cv2.pyrDown(mask)
-    frame2 = cv2.pyrDown(frame)
-    white_x = []
-    white_y = []
-    for y in xrange( mask.shape[0] ):
-        for x in xrange( mask.shape[1] ):
-            if mask.item(y,x) == 255:
-                white_x.append(x)
-                white_y.append(y)
-
-    avg_y = sum(white_y) / float(len(white_y)+0.1)
-    avg_x = sum(white_x) / float(len(white_x)+0.1)
-
-    color = cv2.mean(frame)[:3]
-    #color = cv2.max(frame,frame)[:3]
-    #color = cv2.mean(color)[:3]
-    #color = (10,100,10)
-    if avg_x != 0 and avg_y != 0:  
-        cv2.circle(frame2, (int(avg_x), int(avg_y)), 10, color, -1) 
-
-    cv2.imshow('frame', frame2)
-    cv2.imshow('mask', mask)
-
+        self.frame_old = frame
     
-    frame_old = frame
+        return avg_y
+        #return (avg_x, avg_y)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    def destroy(self):
+        cap.release()
+        cv2.destroyAllWindows()
