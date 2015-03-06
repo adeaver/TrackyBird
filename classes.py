f1a6b35529da4c0e60b2b95857a94530028954b1
byronwasti <byron.wasti@gmail.com>
2015-03-06 14:52:04 -0500

    Combined all files into one, classes.py

---
+++ classes.py
@@ -0,0 +1,135 @@
+import pygame
+from math import cos, pi
+import Rectangle
+
+jumping_steps = 20
+
+class Bird():
+    """ Represents the player in the game (the Tracky Bird) """
+    def __init__(self,screenx, screeny):
+        """ Initialize a Tracky bird at the specified position
+            pos_x, pos_y """
+        self.image = pygame.image.load("./images/bird.gif")
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.pos_x = screenx/2
+        self.pos_y = screeny/2
+        self.death_height = int(screeny * (7.0/8.0))
+        self.width = 50 # replace with width of sprite
+        self.height = 50 # replace with height of sprite
+        self.v_x = 0
+        self.v_y = 0
+        self.jumping = 0
+        # TODO: don't depend on relative path
+        #self.image = pygame.image.load('images/bird_wing_up.png')
+        #self.image.set_colorkey((255,255,255))
+
+    def get_drawables(self):
+        """ get the drawables that makeup the Flappy Bird Player """
+        return [DrawableSurface(self.image, self.image.get_rect().move(self.pos_x, self.pos_y))]
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.pos_x, self.pos_y))
+
+    def update(self, delta_t):
+        """ update the flappy bird's position """
+        if self.jumping > 5:
+            self.v_y = -200 * cos((self.jumping/jumping_steps)*2*pi)
+            self.jumping -=1
+        elif self.jumping > 0:
+            self.v_y += 20
+            self.jumping -= 1
+        else:
+            # self.v_y = delta_t*400 # this is gravity in pixels / s^2
+            self.v_y = 200
+
+        self.pos_x += self.v_x*delta_t
+        self.pos_y += self.v_y*delta_t
+
+    def check_loss(self):
+        return (self.pos_y > self.death_height - 30)
+
+    def collision(self, rectangles):
+        bird_rect = self.get_rect()
+        for rect in rectangles:
+            if rect.contains(bird_rect):
+                return True
+            elif (rect.get_left() <= self.pos_x and rect.get_right() >= self.pos_x and self.pos_y <= 0):
+                return True
+        return False
+
+    def get_rect(self):
+        return Rectangle.Rectangle(self.pos_x, self.pos_y, self.width, self.height)
+
+    def flap(self):
+        """ cause the bird to accelerate upwards (negative y direction) """
+        # self.v_y = -200
+        if(self.pos_y > 40):
+            self.jumping = jumping_steps
+
+class PipeObstacle():
+
+    counter = 0
+
+    def __init__(self, screenx, screeny):
+        PipeObstacle.counter += 1
+        self.count = PipeObstacle.counter
+        self.screenx = screenx
+        self.screeny = screeny
+        self.posx = screenx + (((self.screenx)/2+60) * (self.count-1))
+        print self.posx
+        self.width = 120
+        self.posy = random.randrange(screeny-300, int(screeny * (7.0/8.0)))
+        self.height = screeny-self.posy
+        self.image = pygame.image.load('./images/pipe_body.png')
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.reflection = pygame.transform.scale(self.image, (self.width, self.posy-200))
+
+    def draw(self, screen):
+        # pygame.draw.rect(screen, pygame.Color(255,0,0), (self.posx, self.posy, self.width, self.height))
+        # pygame.draw.rect(screen, pygame.Color(255, 0, 0), (self.posx, 0, self.width, self.posy-200))
+        screen.blit(self.image, (self.posx, self.posy))
+        screen.blit(self.reflection, (self.posx, 0))
+
+    def update(self, delta_t):
+        self.posx -= (200 * delta_t)
+
+        if(self.posx < -1 * self.width):
+            self.posx = self.screenx
+            self.posy = random.randrange(self.screeny-300, int(self.screeny * (7.0/8.0)))
+            self.height = self.screeny-self.posy
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            self.reflection = pygame.transform.scale(self.image, (self.width, self.posy-200))
+
+    def get_x(self):
+        print self.posx
+
+    def rect(self):
+        return [Rectangle.Rectangle(self.posx, self.posy, self.width, self.height), Rectangle.Rectangle(self.posx, 0, self.width, self.posy-200)]
+
+class Rectangle():
+
+    def __init__(self, left, top, width, height):
+        self.top = top
+        self.left = left
+        self.width = width
+        self.height = height
+
+    def contains(self, rect):
+        if(rect.get_right() >= self.left and rect.get_left() <= self.left+self.width):
+            if(rect.get_top() <= self.top+self.height and rect.get_bottom() >= self.top):
+                return True
+
+        return False
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.left + self.width
+
+    def get_top(self):
+        return self.top
+
+    def get_bottom(self):
+        return self.top + self.height
+
